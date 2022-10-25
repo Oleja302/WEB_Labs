35b80a7c29b6b4a35423f0e09fc5b3d415502d08
Oleja302 <wetemez@gmail.com>
2022-10-25 12:50:23 +0300

    Lab #3

---
+++ Lab_4/1/service-worker.js
@@ -0,0 +1,65 @@
+// -----------------------------------------------------------------
+// Template Name: Suha - Multipurpose Ecommerce Mobile HTML Template
+// Template Author: Designing World
+// Template Author URL: https://themeforest.net/user/designing-world
+// -----------------------------------------------------------------
+
+const staticCacheName = 'pre-cache-v2.6.0';
+const dynamicCacheName = 'runtime-cache-v2.6.0';
+
+// Pre Caching Assets
+const precacheAssets = [
+    '/',
+    'css/',
+    'img/',
+    'js/jquery.min.js',
+    'js/pwa.js',
+    'js/dark-mode-switch.js',
+    'js/active.js',
+    'home.html',
+    'manifest.json',
+    'offline.html',
+    'message.html',
+    'cart.html',
+    'pages.html',
+    'settings.html',
+    'style.css'
+];
+
+// Install Event
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open(staticCacheName).then(function (cache) {
+            return cache.addAll(precacheAssets);
+        })
+    );
+});
+
+// Activate Event
+self.addEventListener('activate', function (event) {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(keys
+                .filter(key => key !== staticCacheName && key !== dynamicCacheName)
+                .map(key => caches.delete(key))
+            );
+        })
+    );
+});
+
+// Fetch Event
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request).then(cacheRes => {
+            return cacheRes || fetch(event.request).then(response => {
+                return caches.open(dynamicCacheName).then(function (cache) {
+                    cache.put(event.request, response.clone());
+                    return response;
+                })
+            });
+        }).catch(function() {
+            // Fallback Page, When No Internet Connection
+            return caches.match('offline.html');
+          })
+    );
+});
